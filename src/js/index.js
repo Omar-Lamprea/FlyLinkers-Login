deef15b53c73d6a07fd53c597746a6909ccb7932
unknown <lampreaomar@gmail.com>
2021-07-22 18:48:19 -0500

    added new desktop version

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded',(e)=>{
+//   console.log(screen.width, screen.height)
+//   headerBtns = document.querySelector('.slogan')
+//   console.log(headerBtns)
+
+//   if(window.matchMedia("(max-width: 768px)").matches){
+//     console.log('hola')
+//   }
+
+
+  // if(screen.width > 768){
+  //   console.log('hola')
+  // }
+// })
+
